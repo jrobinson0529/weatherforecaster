4350272b7e1d2e0890eb0d6e219685a81d7c1825
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-25 19:02:05 -0500

    finished up

--- src/javascripts/components/weatherCard.js
+++ src/javascripts/components/weatherCard.js
@@ -1,13 +1,16 @@
-const weatherCard = (object) => {
-  $('#forecast-container').append(`<div class="card city-weather-card" style="width: 18rem;" id=${object.id}>
-                                    <button type="button" class="btn btn-outline-dark delete-btn" id="delete-card">&#10005</button>
-                                    <div class="card-img-top" style="background-image: url('http://openweathermap.org/img/wn/${object.weather[0].icon}@2x.png');">
-                                    </div>
-                                    <div="card-body">
-                                    <h2 class="title" id="title-input-${object.id}" class="title"></h2>
-                                      <p class="card-text">Currently <strong>${object.main.temp}</strong>&#8457 | ${object.weather[0].description}</p>
-                                    </div>
-                                  </div>`);
+const weatherCard = (array) => {
+  $('#forecast-container').empty();
+  array.forEach((element) => {
+    $('#forecast-container').append(`<div class="card city-weather-card" style="width: 18rem;" id="${element.cityId}">
+    <button type="button" class="btn btn-outline-dark delete-btn" id="delete-card--${element.firebaseKey}">&#10005</button>
+   <div class="card-img-top" style="background-image: url('${element.image}');">
+  </div>
+      <div="card-body">
+  <h2 class="title">${element.cityName}</h2>
+   <p class="card-text">Currently <strong>${element.currentTemp}</strong>&#8457 | ${element.weatherDescription}</p>
+ </div>
+</div>`);
+  });
 };
 
 export default weatherCard;
--- src/javascripts/helpers/data/weatherData.js
+++ src/javascripts/helpers/data/weatherData.js
@@ -1,11 +1,36 @@
 import axios from 'axios';
-import { openWeatherConfig } from '../apiKeys';
+import { firebaseConfig, openWeatherConfig } from '../apiKeys';
 
-const dbUrl = openWeatherConfig.weatherDatabaseURL;
+const dbUrl = firebaseConfig.databaseURL;
+const weatherDbUrl = openWeatherConfig.weatherDatabaseURL;
 const getCurrentWeatherData = (city) => new Promise((resolve, reject) => {
-  axios.get(`${dbUrl}/data/2.5/weather?q=${city},us&APPID=${openWeatherConfig.weatherApiKey}&units=imperial`)
+  axios.get(`${weatherDbUrl}/data/2.5/weather?q=${city},us&APPID=${openWeatherConfig.weatherApiKey}&units=imperial`)
     .then((response) => resolve(response.data))
     .catch((error) => reject(error));
 });
+const getUserWeatherData = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/city.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+const createUserWeatherData = (object, uid) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/city.json`, object)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/city/${response.data.name}.json`, body);
+    })
+    .then(() => getUserWeatherData(uid).then((array) => resolve(array)))
+    .catch((error) => reject(error));
+});
+const deleteUserWeatherData = (firebaseKey, uid) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/city/${firebaseKey}.json`)
+    .then(() => getUserWeatherData(uid).then((response) => resolve(response)))
+    .catch((error) => reject(error));
+});
 
-export default getCurrentWeatherData;
+export {
+  createUserWeatherData,
+  getCurrentWeatherData,
+  getUserWeatherData,
+  deleteUserWeatherData
+};
